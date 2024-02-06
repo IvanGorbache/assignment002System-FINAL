df3648dc9d8c840eae8be015371206e8992043b3
ivanGorbache <ivan.gorbache@msmail.ariel.ac.il>
2024-02-06 10:54:06 +0200

    Fix

--- main.c
+++ main.c
@@ -6,42 +6,45 @@ void debugPrinter(int[SIZE][SIZE]);
 
 int main(int argc, char **argv)
 {
-    int mat[SIZE][SIZE];
-	FILE *f;
-	f = fopen(argv[1], "r"); 
-	int i=0, j=0, matrixInput[INPUT_SIZE], loop = TRUE;
-	char option;
-	while (fscanf(f, "%c ", &option)!=EOF && loop == TRUE){
-		switch (option) {
-            case 'A':
-				for (int i = 0; i < INPUT_SIZE; i++) {
-					fscanf(f, "%d ", &matrixInput[i]);
-                    //printf("%d ",matrixInput[i]);
-            	}
-                inputMatrix(mat,matrixInput);
-                break;
+    if(argc=>1)
+    {   
+            int mat[SIZE][SIZE];
+	        FILE *f;
+	        f = fopen(argv[1], "r"); 
+	        int i=0, j=0, matrixInput[INPUT_SIZE], loop = TRUE;
+	        char option;
+	        while (fscanf(f, "%c ", &option)!=EOF && loop == TRUE){
+		        switch (option) {
+                    case 'A':
+				        for (int i = 0; i < INPUT_SIZE; i++) {
+					        fscanf(f, "%d ", &matrixInput[i]);
+                            //printf("%d ",matrixInput[i]);
+                    	}
+                        inputMatrix(mat,matrixInput);
+                        break;
 
-            case 'B':
-				fscanf(f, "%d ", &i);
-				fscanf(f, "%d ", &j);
-                pathExists(mat,i,j)?printf("True\n"):printf("False\n");
-                break;
+                    case 'B':
+				        fscanf(f, "%d ", &i);
+				        fscanf(f, "%d ", &j);
+                        pathExists(mat,i,j)?printf("True\n"):printf("False\n");
+                        break;
 
-            case 'C':
-				fscanf(f, "%d ", &i);
-				fscanf(f, "%d ", &j);
-                printf("%d\n",shortestPath(mat,i,j));
-                break;
+                    case 'C':
+				        fscanf(f, "%d ", &i);
+				        fscanf(f, "%d ", &j);
+                        printf("%d\n",shortestPath(mat,i,j));
+                        break;
 
-            case 'D':
-				loop = FALSE;
-                break;
+                    case 'D':
+				        loop = FALSE;
+                        break;
 
-            default:
-                continue;
-        }
-	}
-	fclose(f);
+                    default:
+                        continue;
+                }
+	        }
+	        fclose(f);
+    }
     return 0;
 }
 
