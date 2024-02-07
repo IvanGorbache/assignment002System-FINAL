b41af225103444ce56edeb47d7812ec14e152c89
ivanGorbache <ivan.gorbache@msmail.ariel.ac.il>
2024-02-07 13:07:34 +0200

    Remvoed prints from sack

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -7,24 +7,19 @@
 
 
 int knapSack(int[MAX_LEN], int[MAX_LEN], int[MAX_LEN]);
-void fillResults(char[MAX_LEN], int[MAX_LEN], char[MAX_LEN]);
+
 void getInputs(char[MAX_LEN], int[MAX_LEN], int[MAX_LEN]);
-void printResults(int profit, char result[MAX_LEN]);
+void printResults(int, char[MAX_LEN], int[MAX_LEN]);
 
 int main()
 {
 	char items[MAX_LEN];
-	char result[MAX_LEN] = {0,0,0,0,0};
 	int values[MAX_LEN];
 	int weights[MAX_LEN];
 	int selected_bool[MAX_LEN] = {0,0,0,0,0};
-	int profit = 0;
 	getInputs(items, weights, values);
 	
-	profit = knapSack(weights, values, selected_bool);
-	fillResults(items, selected_bool, result);
-
-	printResults(profit,result);
+	printResults(knapSack(weights, values, selected_bool),items,selected_bool);
 	return 0;
 }
 
@@ -69,54 +64,26 @@ int knapSack(int weights[MAX_LEN], int values[MAX_LEN], int selected_bool[MAX_LE
 	return table[MAX_LEN][MAX_WEIGHT];
 }
 
-void fillResults(char items[MAX_LEN], int selected_bool[MAX_LEN], char result[MAX_LEN])
-{
-	int k = 0;
-	for(int i = 0; i < MAX_LEN; i++)
-	{
-		if(selected_bool[i])
-		{
-			result[k++] = items[i];
-		}
-	}
-}
-
 void getInputs(char items[MAX_LEN], int weights[MAX_LEN], int values[MAX_LEN])
 {
-	printf("\nPlease input values for %d item(s) (knapsack capacity: %d):",MAX_LEN,MAX_WEIGHT);
 	for(int i = 0; i < MAX_LEN; i++)
 	{
-		printf("\nEnter name for item number (%d):",i+1);
 		scanf(" %c",&items[i]);
-	}
-	for(int i = 0; i < MAX_LEN; i++)
-	{
-		printf("\nEnter value for item (%c):",items[i]);
 		scanf("%d",&values[i]);
-	}
-	for(int i = 0; i < MAX_LEN; i++)
-	{
-		printf("\nEnter weight for item (%c):",items[i]);
 		scanf("%d",&weights[i]);
 	}
-	printf("\nThank you! Happy trails!");
 }
 
-void printResults(int profit, char result[MAX_LEN])
+void printResults(int profit, char items[MAX_LEN], int selected_bool[MAX_LEN])
 {
-	printf("\nMaximum profit: %d", profit);
-	printf("\nItems that give the maximum profit: [");
-	for(int i = MAX_LEN-1; i >= 0; i--)
+	printf("Maximum profit: %d", profit);
+	printf("Selected items:");
+	for(int i = 0; i < MAX_LEN; i++)
 	{
-		if(result[i]!=0)
+		if(selected_bool[i]==1)
 		{
-			printf("%c",result[i]);
-			if(i-1>=0 && result[i-1]!=' ')
-			{
-				printf(", ");
-			}
+			printf(" %c",items[i]);
 		}
 	}
-	printf("]\n");
 }
 
